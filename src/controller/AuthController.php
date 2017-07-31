f77124f63b97c9122650d22600dfbc13ba08ae03
phoro3 <puhro20@gmail.com>
2017-07-31 22:52:47 +0900

    Add error message when inputs are null

--- src/controller/AuthController.php
+++ src/controller/AuthController.php
@@ -19,19 +19,24 @@ class AuthController{
         $userId = $params['userId'];
         $password = $params['password'];
 
-        try{
-            $userModel = new User($this->container->db);
-            $auth = new AuthLogic();
+        if($userId and $password){
+            try{
+                $userModel = new User($this->container->db);
+                $auth = new AuthLogic();
 
-            if($auth->isLogin($userModel, $userId, $password)){
-                return $response->withStatus(302)->withHeader('Location', '/');
-            }else{
-                $args['errorMessage'] = 'ユーザーIDまたはパスワードが違います';
-                return $this->container->renderer->render($response, 'login.php', $args);
+                if($auth->isLogin($userModel, $userId, $password)){
+                    return $response->withStatus(302)->withHeader('Location', '/');
+                }else{
+                    $args['errorMessage'] = 'ユーザーIDまたはパスワードが違います';
+                    return $this->container->renderer->render($response, 'login.php', $args);
+                }
+            }catch(\PDOException $e){
+                $args['errorMessage'] = 'エラーが発生しました';
+                return $this->container->renderer->render($response, 'error.php', $args);
             }
-        }catch(\PDOException $e){
-            $args['errorMessage'] = 'エラーが発生しました';
-            return $this->container->renderer->render($response, 'error.php', $args);
+        }else{
+            $args['errorMessage'] = 'ユーザーIDとパスワードの両方を入力してください';
+            return $this->container->renderer->render($response, 'login.php', $args);
         }
     }
 
