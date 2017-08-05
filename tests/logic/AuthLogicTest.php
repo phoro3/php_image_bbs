3bc26b79d600478362e3972263f2d5b08ba4ea88
phoro3 <puhro20@gmail.com>
2017-08-05 17:20:42 +0900

    Add test for invalid case

--- tests/logic/AuthLogicTest.php
+++ tests/logic/AuthLogicTest.php
@@ -36,5 +36,42 @@ class AuthLogicTest extends \PHPUnit_Framework_TestCase{
         $this->assertEquals($userData[0]['userId'], $_SESSION['userId']);
         $this->assertEquals($userData[0]['name'], $_SESSION['name']);
         $this->assertTrue($_SESSION['isLogin']);
+
+        //When password is incorrect
+        $_SESSION['userId'] = null;
+        $_SESSION['name'] = null;
+        $_SESSION['isLogin'] = null;
+        $this->assertNull($_SESSION['userId']);
+        $this->assertNull($_SESSION['name']);
+        $this->assertNull($_SESSION['isLogin']);
+        $return = $logic->isLogin($userMock, $userId, 'incorrectpassword');
+        $this->assertFalse($return);
+        $this->assertNull($_SESSION['userId']);
+        $this->assertNull($_SESSION['name']);
+        $this->assertNull($_SESSION['isLogin']);
+    }
+
+    /**
+     * Test for isLogin when exception happens
+     */
+    public function testIsLoginWithException(){
+        $logic = new AuthLogic();
+
+        //Test data
+        $userId = 'test';
+        $password = 'testpassword';
+
+        //Create mock
+        $userMock = \Mockery::mock('User');
+        $userMock
+            ->shouldReceive('searchUserById')
+            ->andThrow(new \PDOException());
+
+        try{
+            $result = $logic->isLogin($userMock, $userId, $password);
+            $this->fail();
+        }catch(\PDOException $e){
+            $this->assertNotNull($e);
+        }
     }
 }
